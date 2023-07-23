b99d39b36c2860b0ef1c89b46b61a8da99670f53
FranAlamo <alamofrancis@gmail.com>
2023-07-23 11:19:25 -0300

    api error

--- src/Components/Api.jsx
+++ src/Components/Api.jsx
@@ -1,14 +1,20 @@
 import { useFetch } from "../../src/useFetch";
 function Api() {
-  const { data } = useFetch("https://jsonplaceholder.typicode.com/todos");
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
   return (
     <div className="App">
       <h1>Aqui se muestra la Api</h1>
-      <ul className="card">
-        {data?.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <div className="card">
+        <ul>
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading....</li>}
+          {data?.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
